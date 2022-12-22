2550984747b9ef8c9b993fa95547e09fff90a609
LavinoS <vasile.mihalache99@gmail.com>
2022-12-22 11:20:22 +0200

    Deleted useless files

--- dist/models/users/routes/getAllUsersRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import getAllUsersController from '../controllers/getAllUsersController.js';
-const router = express.Router();
-router.get('/users', getAllUsersController);
-export default router;
